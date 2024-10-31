1e9447e3ff8d66f9df911aa81a082b2b160368e0
rlozl15 <eunjikim.dev@gmail.com>
2024-10-31 23:52:42 +0900

    feat: #3 로그인 기능 구현
    - form 클래스를 사용해 로그인 페이지 구성
    - view에 전달된 데이터를 form으로 처리하고 로그인 유효성을 확인하여 로그인 처리
    - 로그인 실패시 message 띄우기

---
+++ users/forms.py
@@ -0,0 +1,5 @@
+from django import forms
+
+class LoginForm(forms.Form):
+    username = forms.CharField(min_length=3)
+    password = forms.CharField(min_length=4)
--- users/views.py
+++ users/views.py
@@ -1,6 +1,33 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from users.forms import LoginForm
+from django.contrib import messages
 
 def login_view(request):
     if request.user.is_authenticated:
         return redirect("/posts/feeds/")
-    return render(request, "users/login.html")
+
+    if request.method == "POST":
+        form = LoginForm(data=request.POST)
+        # 유효성 검사
+        if form.is_valid():
+            username = form.cleaned_data["username"]
+            password = form.cleaned_data["password"]
+
+            # username과 password에 해당하는 사용자가 있는지 검사
+            user = authenticate(username=username, password=password)
+
+            # 사용자가 있다면 로그인 처리
+            if user:
+                login(request, user)
+                return redirect("/posts/feeds/")
+            else:
+                messages.error(request, "실패했습니다.")
+    else:
+        form = LoginForm()
+
+    context = {
+        "form": form,
+    }
+
+    return render(request, "users/login.html", context)
