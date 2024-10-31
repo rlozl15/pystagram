7ae9d541ea980211c727e4c2657d8244f54e7266
rlozl15 <eunjikim.dev@gmail.com>
2024-10-31 23:19:21 +0900

    feat: #3 로그인 여부에 따른 접속 제한
    - admin을 사용하여 로그인 여부를 판단하고, 그에 따른 페이지 이동을 구현함
    - index.html은 더 이상 사용하지 않음으로 삭제

--- posts/views.py
+++ posts/views.py
@@ -1,4 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def feeds(request):
+    # 로그인 되어 있지 않으면 로그인 페이지로 이동
+    if not request.user.is_authenticated:
+        return redirect("/users/login/")
     return render(request, "posts/feeds.html")
--- users/views.py
+++ users/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 def login_view(request):
+    if request.user.is_authenticated:
+        return redirect("/posts/feeds/")
     return render(request, "users/login.html")
