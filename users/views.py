a8a6cd9d8374120091c74c8bc8f5c916aeeea17b
rlozl15 <eunjikim.dev@gmail.com>
2024-11-01 02:20:16 +0900

    feat: #3 회원가입 구현
    - 회원가입 form 클래스를 정의하고 회원가입 페이지 구성

--- users/views.py
+++ users/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
-from users.forms import LoginForm
+from users.forms import LoginForm, SignupForm
 
 def login_view(request):
     if request.user.is_authenticated:
@@ -36,3 +36,10 @@ def login_view(request):
 def logout_view(request):
     logout(request)
     return redirect("/users/login/")
+
+def signup(request):
+    form = SignupForm()
+    context = {
+        "form": form
+    }
+    return render(request, "users/signup.html", context)
