a7ae4ac412f43bd3ea8891008be7306b229b52a5
rlozl15 <eunjikim.dev@gmail.com>
2024-11-01 12:54:30 +0900

    feat: #5 admin에 연관 객체 표시(이미지, 댓글, 썸네일)
    - 관리자 Post 페이지에서 ForeignKey로 연결된 다른 객체를 확인할 수 있도록 추가
    - 관리자 Post 페이지 내 이미지에 대한 썸네일 추가

--- posts/admin.py
+++ posts/admin.py
@@ -1,9 +1,20 @@
 from django.contrib import admin
 from posts.models import Post, PostImage, Comment
+import admin_thumbnails
+
+class CommentInline(admin.TabularInline):
+    model = Comment
+    extra = 1
+
+@admin_thumbnails.thumbnail("photo")
+class PostImangeInline(admin.TabularInline):
+    model = PostImage
+    extra = 1
 
 @admin.register(Post)
 class PostAdmin(admin.ModelAdmin):
     list_display = ["id", "content"]
+    inlines = [CommentInline, PostImangeInline]
 
 @admin.register(PostImage)
 class PostImangeAdmin(admin.ModelAdmin):
