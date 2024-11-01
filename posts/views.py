43628008b3cdc74e479f8eaf49afaaae21d824c4
rlozl15 <eunjikim.dev@gmail.com>
2024-11-01 23:18:52 +0900

    feat: #6 글 작성 페이지 구현
    - PostForm을 사용하여 글 작성 내용 전달 및 저장
    - 여러 이미지를 별도로 PostImage에 저장

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
-from posts.models import Post, Comment
-from posts.forms import CommentForm
+from posts.models import Post, Comment, PostImage
+from posts.forms import CommentForm, PostForm
 from django.views.decorators.http import require_POST
 from django.http import HttpResponseRedirect, HttpResponseForbidden
 
@@ -37,3 +37,25 @@ def comment_delete(request, comment_id):
         comment.delete()
         return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}")
     return HttpResponseForbidden("이 댓글을 삭제할 권한이 없습니다.")
+
+def post_add(request):
+    if request.method == "POST":
+        form = PostForm(data=request.POST)
+
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.user = request.user
+            post.save()
+
+            for image_file in request.FILES.getlist("images"):
+                PostImage.objects.create(
+                    post=post,
+                    photo=image_file,
+                )
+
+            url = f'/posts/feeds/#post-{post.id}'
+            return HttpResponseRedirect(url)
+    else:
+        form = PostForm()
+    context = {"form":form}
+    return render(request, "posts/post_add.html", context)
