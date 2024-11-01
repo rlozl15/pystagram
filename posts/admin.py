0d0f0a4e13412a33810f678db5dab3db457336cf
rlozl15 <eunjikim.dev@gmail.com>
2024-11-01 12:13:50 +0900

    feat: #5 글, 이미지, 댓글 모델링
    - 데이터 모델 구성
    - 관리자 기능 구성

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from posts.models import Post, PostImage, Comment
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ["id", "content"]
+
+@admin.register(PostImage)
+class PostImangeAdmin(admin.ModelAdmin):
+    list_display = ["id", "post", "photo"]
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ["id", "post", "content"]
