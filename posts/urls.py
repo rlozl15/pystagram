7026004652a1fb6771ac4f8c2983e79ff81f3ace
rlozl15 <eunjikim.dev@gmail.com>
2024-11-01 16:19:35 +0900

    feat: #6 댓글 작성 페이지 구현
    - Form을 사용하여 댓글 정보를 받고, 내부적으로 사용자 정보까지 추가하여 댓글을 저장한다
    - 댓글수, 댓글 삭제 기능 추가

--- posts/urls.py
+++ posts/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
-from posts.views import feeds
+from posts.views import feeds, comment_add, comment_delete
 
 urlpatterns = [
     path("feeds/", feeds),
+    path("comment_add/", comment_add),
+    path("comment_delete/<int:comment_id>/", comment_delete),
 ]
--- posts/views.py
+++ posts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render, redirect
-from posts.models import Post
+from posts.models import Post, Comment
+from posts.forms import CommentForm
+from django.views.decorators.http import require_POST
+from django.http import HttpResponseRedirect, HttpResponseForbidden
 
 def feeds(request):
     # 로그인 되어 있지 않으면 로그인 페이지로 이동
@@ -8,5 +11,29 @@ def feeds(request):
 
     # 모든 글 목록 전달
     posts = Post.objects.all()
-    context = {"posts":posts}
+    comment_form = CommentForm()
+    context = {
+        "posts":posts,
+        "comment_form":comment_form,
+    }
     return render(request, "posts/feeds.html", context)
+
+@require_POST
+def comment_add(request):
+    form = CommentForm(data=request.POST)
+    if form.is_valid():
+        # 메모리상에 Comment 객체 생성
+        comment = form.save(commit=False)
+        # 사용자 정보 할당
+        comment.user = request.user
+        # DB에 저장
+        comment.save()
+        return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}")
+
+@require_POST
+def comment_delete(request, comment_id):
+    comment = Comment.objects.get(id=comment_id)
+    if comment.user == request.user:
+        comment.delete()
+        return HttpResponseRedirect(f"/posts/feeds/#post-{comment.post.id}")
+    return HttpResponseForbidden("이 댓글을 삭제할 권한이 없습니다.")
