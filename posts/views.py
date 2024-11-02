589e6c2c4fedda4b5b578a4ef8957dd390d4eb31
rlozl15 <eunjikim.dev@gmail.com>
2024-11-02 15:47:16 +0900

    feat: #8 해시태그 생성 및 글에 추가하는 기능 구현
    - 해시태그를 글에 추가하는 기능 구현

--- posts/views.py
+++ posts/views.py
@@ -56,6 +56,13 @@ def post_add(request):
                     photo=image_file,
                 )
 
+            tag_string = request.POST.get("tags")
+            if tag_string:
+                tag_names = [tag_name.strip() for tag_name in tag_string.split("#")]
+                for tag_name in tag_names[1:]:
+                    tag, _ = HashTag.objects.get_or_create(name=tag_name)
+                    post.tags.add(tag)
+
             url = reverse("posts:feeds") + f"#post-{post.id}"
             return HttpResponseRedirect(url)
     else:
